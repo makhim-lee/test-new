7b6e072bf896ff76b4ff56974e4fecc8238ef465
makhim-lee <yjsm2818@gmail.com>
2023-05-05 12:27:43 +0900

    HRD22

--- local/HDR__/python/shape/test_shape.py
+++ local/HDR__/python/shape/test_shape.py
@@ -1,26 +1,27 @@
-#test_shape.py
+# test_shape.py
 from shape.rectangle import Rectangle
 from shape.circle import Circle
 
-#from shape import *
-#rectangle.Rectangle
+# from shape import *
+# rectangle.Rectangle
+
 
 def printArea(shape):
     if type(shape) == Rectangle:
         s = "rectangle diagonal : "
         s += f"{shape.get_diagonal():.2f}"
     elif type(shape) == Circle:
-        s = "cirecle circumference : "
+        s = "circle circumference : "
         s += f"{shape.get_circumference():.2f}"
     else:
         pass
     print(s, "area : ", shape.area())
 
-    
+
 shapes = [
-    Rectangle(0, 0, 100, 5), 
-    Rectangle(10, 10, 5, 20), 
-    Circle(50, 50, 5), 
+    Rectangle(0, 0, 100, 5),
+    Rectangle(10, 10, 5, 20),
+    Circle(50, 50, 5),
     Rectangle(50, 50, 5, 20),
 ]
 
